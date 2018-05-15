244190316dc99757f386d1ab4f856d0a4688579a
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-05-15 13:59:47 -0400

    Declare function with const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button.heading')
 const hdr = document.querySelector('h1.heading')
 const hdr2 = document.querySelector('h2.heading2')
 
-function changeText(ev) {
+const changeText = function(ev) {
     //ev.target.textContent = 'New User Directory'
     //hdr.textContent = 'New User Directory'   
     hdr2.textContent = 'New second heading' 
