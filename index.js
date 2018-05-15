ea2dd039f7088366b46f2c7cd38ad874e9e5616c
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-05-15 14:42:05 -0400

    Add multiple user names

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function changeMainHeader(ev) {
 const form = document.querySelector('#userForm')
 const handleSubmit = function(ev) {
     ev.preventDefault()
-    const heading = document.querySelector('h1')
+    const users = document.querySelector('#users')
     const f = ev.target
     const userName = f.userName.value
-    heading.textContent = userName
+    users.textContent += ' ' + userName
 }
 form.addEventListener('submit', handleSubmit)
