b94e5e8dd51aea2c45e222932bf33f7cf8bb816f
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-05-14 20:31:16 -0400

    Change second header when button is clicked

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const button = document.querySelector('button.heading')
-//const hdr = document.getElementById('heading').innerHTML
 const hdr = document.querySelector('h1.heading')
+const hdr2 = document.querySelector('h2.heading2')
 
 function changeText(ev) {
     //ev.target.textContent = 'New User Directory'
-    hdr.textContent = 'New User Directory'    
+    //hdr.textContent = 'New User Directory'   
+    hdr2.textContent = 'New second heading' 
 }
 
 button.addEventListener('click', changeText)
